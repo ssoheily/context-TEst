2e23f5afe6caabd73f783c0a28ff55b6fa5c63ce
ssoheily <sam.soheil55@gmail.com>
2022-08-13 21:11:48 +0200

    clean code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-
 import React from "react";
 import Navbar from "./Component/Navbar/Navbar";
-import ProductsSection   from "./Component/ProductsSection/ProductsSection";
-import "./App.css"
+import ProductsSection from "./Component/ProductsSection/ProductsSection";
+import "./App.css";
 
 export default function App() {
-
   return (
-        <div className="test">
-          <Navbar />
-          
-          <main className="pb-5">
-            <div className="container">
-              <h1 className="text-center main-title">
-                All Products
-              </h1>
-              <ProductsSection title="Phone" />
-              <ProductsSection title="Laptop" />
-              <ProductsSection title="Monitor" />
-              <ProductsSection title="Tablet" />
-            </div>
-          </main>
-        </div> 
+    <div className="test">
+      <Navbar />
+
+      <main className="pb-5">
+        <div className="container">
+          <h1 className="text-center main-title">All Products</h1>
+          <ProductsSection title="Phone" />
+          <ProductsSection title="Laptop" />
+          <ProductsSection title="Monitor" />
+          <ProductsSection title="Tablet" />
+        </div>
+      </main>
+    </div>
   );
 }
--- src/Component/ProductsSection/ProductsSection.js
+++ src/Component/ProductsSection/ProductsSection.js
@@ -9,16 +9,25 @@ export default function ProductsSection({ title }) {
       <div className="col-xl-3 col-lg-4 col-md-5 col-sm-10 mt-5">
         <div className="card py-3 px-3">
           <div className="col-12 text-center">
-            <img src="/images/item-1.jpeg" alt="product Image" className="card-img-top w-75 " />
+            <img
+              src="/images/item-1.jpeg"
+              alt="product Image"
+              className="card-img-top w-75 "
+            />
           </div>
-           <div className=" card-body text-center">
+          <div className=" card-body text-center">
             <p className="card-text">SamsungA58</p>
             <p className="price">1230$</p>
             <br />
-            <a href="#" className="btn btn-danger">Add To Basket</a>
-            <a href="#" className="btn btn-outline-dark mt-3 text-capitalize"> More Information</a>
-           <p className="number"> Number: 15 </p>
-           </div>
+            <a href="#" className="btn btn-danger">
+              Add To Basket
+            </a>
+            <a href="#" className="btn btn-outline-dark mt-3 text-capitalize">
+              {" "}
+              More Information
+            </a>
+            <p className="number"> Number: 15 </p>
+          </div>
         </div>
       </div>
     </div>
