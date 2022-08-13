f917f270b9e708c206d00c81454577bfec51b0af
ssoheily <sam.soheil55@gmail.com>
2022-08-13 18:38:56 +0200

    create component Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 
 import React from "react";
+import Navbar from "./Component/Navbar/Navbar";
 export default function App() {
 
   return (
-        <div className="test">sam.soheil</div>
+        <div className="test">
+          <Navbar />
+        </div> 
   );
 }
---
+++ src/Component/Navbar/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Navbar.css"
+import {BsBag} from "react-icons/bs"
+export default function Navbar() { 
+  return (
+    <nav className="navbar navbar-expand-sm  py-3 d-flex">
+      <div className="container">
+        <a href="#" className="navbar-brand">
+          sam.soheil-Shop
+        </a>
+        <ul className="navbar-nav me-auto-me-3">
+          <li className="nav-item">
+            <a href="#  " className="nav-link">
+                Home
+            </a>
+          </li>
+        </ul>
+        <div className="bag-box">
+            <a href="#" className="bag">
+                <BsBag  className="text-white" />
+                <span className="bag-products-counter">0</span>
+            </a>
+        </div>
+      </div>
+    </nav>
+  );
+}
