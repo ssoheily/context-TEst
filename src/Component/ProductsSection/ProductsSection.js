db1a624b49d8be4239d6463fc4c4cdb9c6c779e6
ssoheily <sam.soheil55@gmail.com>
2022-08-13 21:33:59 +0200

    add css to  productsSection.css

--- src/Component/ProductsSection/ProductsSection.js
+++ src/Component/ProductsSection/ProductsSection.js
@@ -4,30 +4,21 @@ import "./ProductsSection.css";
 export default function ProductsSection({ title }) {
   return (
     <div className="row justify-content-center  mt-5">
-      <h3 className="text- center">{title}</h3>
+      <h3 className="text-center">{title}</h3>
       {/* now show product */}
       <div className="col-xl-3 col-lg-4 col-md-5 col-sm-10 mt-5">
         <div className="card py-3 px-3">
           <div className="col-12 text-center">
-            <img
-              src="/images/item-1.jpeg"
-              alt="product Image"
-              className="card-img-top w-75 "
-            />
+            <img src="/images/item-1.jpeg" alt="product Image" className="card-img-top w-75 " />
           </div>
-          <div className=" card-body text-center">
+           <div className=" card-body text-center">
             <p className="card-text">SamsungA58</p>
             <p className="price">1230$</p>
             <br />
-            <a href="#" className="btn btn-danger">
-              Add To Basket
-            </a>
-            <a href="#" className="btn btn-outline-dark mt-3 text-capitalize">
-              {" "}
-              More Information
-            </a>
-            <p className="number"> Number: 15 </p>
-          </div>
+            <a href="#" className="btn btn-danger">Add To Cart</a>
+            <a href="#" className="btn btn-outline-dark mt-3 text-capitalize"> More Information</a>
+           <p className="number"> Number: 15 </p>
+           </div> 
         </div>
       </div>
     </div>
